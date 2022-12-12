7c4eed7580ee19946b9f5673f0bf2c39503b008d
gopal8220 <gopalkumar8220@gmail.com>
2022-12-12 12:54:44 +0530

    initial commit

---
+++ 01 first java program/first-idea-project/src/com/gopal/Primitives.java
@@ -0,0 +1,18 @@
+package com.gopal;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.67f;
+        double largeDecimalNumbers = 456754.4567;
+        long largeInteger = 3456783456876543L;
+        boolean check = false;
+
+
+        float c = 30.6f;
+        c++;
+//        c = (int)(c) + 1;
+        System.out.println(c);
+    }
+}
