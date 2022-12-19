bb25df49ae1c87fffcb28f31c9e47fa646cff3a1
gopal8220 <gopalkumar8220@gmail.com>
2022-12-19 20:47:30 +0530

    initial commit

---
+++ 02 conditional lops/Conditinal/src/Conditional1.java
@@ -0,0 +1,11 @@
+public class Conditional1 {
+    public static void main(String[] args) {
+        int x=5;
+        if(x<6)
+            System.out.print("Hello  ");
+        if(x==5)
+            System.out.print("Hii ");
+        else
+            System.out.print("Hey ");
+    }
+}
