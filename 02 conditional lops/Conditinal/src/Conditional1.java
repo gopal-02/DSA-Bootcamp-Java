8df4de81db50403bd9c8873541f39d3cf0ad021d
gopal8220 <gopalkumar8220@gmail.com>
2022-12-19 21:31:56 +0530

    code updated

--- 02 conditional lops/Conditinal/src/Conditional1.java
+++ 02 conditional lops/Conditinal/src/Conditional1.java
@@ -1,11 +1,20 @@
 public class Conditional1 {
     public static void main(String[] args) {
         int x=5;
-        if(x<6)
-            System.out.print("Hello  ");
-        if(x==5)
-            System.out.print("Hii ");
-        else
-            System.out.print("Hey ");
+//        if(x<6)
+//            System.out.print("Hello  ");
+//        if(x==5)
+//            System.out.print("Hii ");
+//        else
+//            System.out.print("Hey ");
+//
+        int var1=5;
+        int var2=6;
+        if ((var2=1)==var1) {
+            System.out.print(var2);
+        }
+        else{
+            System.out.println(var2+1);
+        }
     }
 }
